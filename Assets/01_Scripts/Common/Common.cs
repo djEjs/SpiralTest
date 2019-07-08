6e65021f936e920c045689785e9fdd0c81499e6a
djEjs <52478827+djEjs@users.noreply.github.com>
2019-07-09 00:21:15 +0900

    Common_20190709_01
    Common

---
+++ Assets/01_Scripts/Common/Common.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+static public class Common
+{
+	static public float PI { get { return Mathf.PI; } }
+	static public float Sin(float angle) { return Mathf.Sin(angle); }
+	static public float Cos(float angle) { return Mathf.Cos(angle); }
+	static public float Tan(float angle) { return Mathf.Tan(angle); }
+	static public float Atan2(float y, float x) { return Mathf.Atan2(y, x); }
+
+	static public Vector3 ConvertPos(Vector2 pos, float rad)
+	{
+		return ConvertPos(pos.x, pos.y, rad);
+	}
+	static public Vector3 ConvertPos(float x, float y, float rad)
+	{
+		float _x = 0;
+		float _y = y;
+		float _z = 0;
+
+		_x = rad * Sin(x);
+		_z = rad * Cos(x);
+
+		return new Vector3(_x, _y, _z);
+	}
+}
---
+++ Assets/01_Scripts/Test.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour
+{
+	[Range(0,100)]
+	public float x;
+	[Range(0, 30)]
+	public float y;
+	[Range(0, 10)]
+	public float rad;
+
+	private void Update()
+	{
+		transform.position = Common.ConvertPos(Common.PI * x * Mathf.Deg2Rad, y, rad);
+	}
+}
